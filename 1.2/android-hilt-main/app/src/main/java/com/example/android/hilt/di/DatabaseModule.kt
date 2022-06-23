c03e86284a38038ffc8091ce00aaca0d705289e4
wily.yang <ehdrnr1178@gmail.com>
2022-06-23 18:01:52 +0900

    2.2 progress

---
+++ 1.2/android-hilt-main/app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
@@ -0,0 +1,10 @@
+package com.example.android.hilt.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object DatabaseModule {
+
+}
---
+++ 1.2/android-hilt-main/app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
@@ -0,0 +1,79 @@
+package com.example.android.hilt.ui
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.hilt.LogApplication
+import com.example.android.hilt.R
+import com.example.android.hilt.data.Log
+import com.example.android.hilt.data.LoggerLocalDataSource
+import com.example.android.hilt.util.DateFormatter
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+
+@AndroidEntryPoint
+class LogsFragment : Fragment() {
+
+    @Inject lateinit var logger: LoggerLocalDataSource
+    @Inject lateinit var dateFormatter: DateFormatter
+
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_logs, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view).apply {
+            setHasFixedSize(true)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        logger.getAllLogs { logs ->
+            recyclerView.adapter =
+                LogsViewAdapter(
+                    logs,
+                    dateFormatter
+                )
+        }
+    }
+}
+
+private class LogsViewAdapter(
+    private val logsDataSet: List<Log>,
+    private val daterFormatter: DateFormatter
+) : RecyclerView.Adapter<LogsViewAdapter.LogsViewHolder>() {
+
+    class LogsViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LogsViewHolder {
+        return LogsViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.text_row_item, parent, false) as TextView
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return logsDataSet.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: LogsViewHolder, position: Int) {
+        val log = logsDataSet[position]
+        holder.textView.text = "${log.msg}\n\t${daterFormatter.formatDate(log.timestamp)}"
+    }
+}
