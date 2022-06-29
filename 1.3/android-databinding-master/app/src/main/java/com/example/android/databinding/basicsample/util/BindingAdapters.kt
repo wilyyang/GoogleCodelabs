1627c9ea790ffb42bc3692b98c628c7056b97798
wily.yang <ehdrnr1178@gmail.com>
2022-06-29 18:17:17 +0900

    1.3 inprogress

--- 1.3/android-databinding-master/app/src/main/java/com/example/android/databinding/basicsample/util/BindingAdapters.kt
+++ 1.3/android-databinding-master/app/src/main/java/com/example/android/databinding/basicsample/util/BindingAdapters.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2018 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.databinding.basicsample.util
 
 import android.content.Context
@@ -29,10 +13,16 @@ import androidx.databinding.BindingAdapter
 import com.example.android.databinding.basicsample.R
 import com.example.android.databinding.basicsample.data.Popularity
 
-/**
- * A Binding Adapter that is called whenever the value of the attribute `app:popularityIcon`
- * changes. Receives a popularity level that determines the icon and tint color to use.
- */
+@BindingAdapter("app:hideifZero")
+fun hideIfZero(view: View, number: Int){
+    view.visibility = if (number == 0) View.GONE else View.VISIBLE
+}
+
+@BindingAdapter(value = ["app:progressScaled", "android:max"], requireAll = true)
+fun setProgress(progressBar: ProgressBar, likes: Int, max: Int){
+    progressBar.progress = (likes * max / 5).coerceAtMost(max)
+}
+
 @BindingAdapter("app:popularityIcon")
 fun popularityIcon(view: ImageView, popularity: Popularity) {
 
@@ -43,10 +33,6 @@ fun popularityIcon(view: ImageView, popularity: Popularity) {
     view.setImageDrawable(getDrawablePopularity(popularity, view.context))
 }
 
-/**
- * A Binding Adapter that is called whenever the value of the attribute `android:progressTint`
- * changes. Depending on the value it determines the color of the progress bar.
- */
 @BindingAdapter("app:progressTint")
 fun tintPopularity(view: ProgressBar, popularity: Popularity) {
 
@@ -55,26 +41,6 @@ fun tintPopularity(view: ProgressBar, popularity: Popularity) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
         view.progressTintList = ColorStateList.valueOf(color)
     }
-}
-
-/**
- *  Sets the value of the progress bar so that 5 likes will fill it up.
- *
- *  Showcases Binding Adapters with multiple attributes. Note that this adapter is called
- *  whenever any of the attribute changes.
- */
-@BindingAdapter(value = ["app:progressScaled", "android:max"], requireAll = true)
-fun setProgress(progressBar: ProgressBar, likes: Int, max: Int) {
-    progressBar.progress = (likes * max / 5).coerceAtMost(max)
-}
-
-/**
- * Unused Binding Adapter to replace the Binding Converter that hides a view if the number
- * of likes is zero.
- */
-@BindingAdapter("app:hideIfZero")
-fun hideIfZero(view: View, number: Int) {
-    view.visibility = if (number == 0) View.GONE else View.VISIBLE
 }
 
 private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
