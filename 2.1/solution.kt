6f73f3dcc0bfc583468a14f04eb4edb0d96da0c4
wily.yang <ehdrnr1178@gmail.com>
2022-07-06 11:18:45 +0900

    2.1 complete

---
+++ 2.1/solution.kt
@@ -0,0 +1,13 @@
+import kotlinx.coroutines.*
+
+fun main() {
+   val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
+   repeat(3) {
+       GlobalScope.launch {
+           println("${Thread.currentThread()} has started")
+           for (i in states) {
+               println("${Thread.currentThread()} - $i")
+           }
+       }
+   }
+}
